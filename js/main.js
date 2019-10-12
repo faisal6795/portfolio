9b2be04c7372b1fd3b32b4f78cf79acc15b106d6
Faisal Ansari <faisal6795@gmail.com>
2019-10-12 23:47:37 +0530

    First commit for portfolio

---
+++ js/main.js
@@ -0,0 +1,76 @@
+function initialize() {
+    var JSONData;
+
+    $.getJSON("data/data.json", function (data) {
+        JSONData = data;
+        initializeElements();
+    });
+
+    function renderTemplates(templateName, index, target, jsonData) {
+        $.get('templates/' + templateName + '.handlebars', function (data) {
+            var template = Handlebars.compile(data);
+            $(target).html(template(jsonData));
+        }, 'html').promise().done(function () {
+            if (index === 8) {
+                bindEvents();
+                initMap();
+            }
+        });
+    }
+
+    function bindEvents() {
+        var openNavBar = function () {
+            $(".nav-container ul").addClass("nav-open");
+            $(".overlay").addClass("active");
+            $(".toggle-btn").addClass("close-btn");
+        }, closeNavBar = function () {
+            $(".nav-container ul").removeClass("nav-open");
+            $(".overlay").removeClass("active");
+            $(".toggle-btn").removeClass("close-btn");
+        };
+
+        $("#navbar .navlink").click(function (event) {
+            $("#navbar .navlink").closest("li").removeClass("selected");
+            $(event.target).closest("li").addClass("selected");
+            closeNavBar();
+        });
+
+        $(".project-nav-container li").click(function (event) {
+            $(".project-nav-container li").removeClass("selected");
+            $(event.target).addClass("selected");
+            var filter = event.target.dataset.filter;
+        });
+
+        $(".toggle-btn").click(function (event) {
+            if ($(".nav-container ul").hasClass("nav-open")) {
+                closeNavBar();
+            } else {
+                openNavBar();
+            }
+        });
+
+        $("#navbar .theme-changer button").click(function (event) {
+            if ($("#portfolio").hasClass("light-mode")) {
+                $("#portfolio").removeClass("light-mode");
+            } else {
+                $("#portfolio").addClass("light-mode");
+            }
+        });
+
+        $("#send-button").click(function (event) {
+
+        });
+    }
+
+    function initializeElements() {
+        JSONData.sections.forEach(function (data, index) {
+            renderTemplates(data, index, '#' + data, JSONData[data]);
+        });
+    }
+
+    function initMap() {
+        var address = { lat: 19.043563, lng: 73.022391 };
+        var map = new google.maps.Map(document.getElementById('map'), { zoom: 12, center: address });
+        var marker = new google.maps.Marker({ position: address, map: map });
+    }
+}
